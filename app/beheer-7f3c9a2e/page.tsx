1f1c0e30c3de15cf8c925773c18b43c5aa156312
kevke1990 <otten.kevin@gmail.com>
2026-09-07 22:49:11 +0200

    style(admin): load control center stylesheet

--- app/beheer-7f3c9a2e/page.tsx
+++ app/beheer-7f3c9a2e/page.tsx
@@ -1,9 +1,11 @@
 import { requireAdmin } from "@/lib/auth";
 import AdminPortal from "./AdminPortal";
+import styles from "./admin.module.css";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPage() {
   const admin = await requireAdmin();
+  void styles;
   return <AdminPortal adminEmail={admin.email} />;
 }
