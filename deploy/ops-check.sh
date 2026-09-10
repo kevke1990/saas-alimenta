235d3a0c6c4b2ef426894cf7de4ad6d6a86a1aac
kevke1990 <otten.kevin@gmail.com>
2026-09-10 13:56:01 +0200

    fix(deploy): use configurable application directory for ops checks

--- deploy/ops-check.sh
+++ deploy/ops-check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-APP_DIR="${APP_DIR:-/opt/alimenta}"
+APP_DIR="${APP_DIR:-/opt/saas-alimenta}"
 cd "$APP_DIR"
 COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.prod.yml}"
 
